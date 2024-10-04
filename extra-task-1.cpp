cd9b86e1155eac2cb226cb11c3b8902526f7bb6f
Ignatov Maksim <omegamax@list.ru>
2024-10-04 21:50:55 +0300

    Task 5

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -2,6 +2,7 @@
 #include <assert.h>
 #include <cmath>
 
+
 double seconds_difference(double time_1, double time_2)
 {
     return time_2 - time_1;
@@ -24,6 +25,7 @@ double seconds_difference(double time_1, double time_2)
     */
 }
 
+
 double hours_difference(double time_1, double time_2)
 {
     return seconds_difference(time_1, time_2) / 3600.0;
@@ -44,6 +46,7 @@ double hours_difference(double time_1, double time_2)
         0.0
     */
 }
+
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
@@ -66,6 +69,7 @@ double to_float_hours(int hours, int minutes, int seconds)
         1.01
     */
 }
+
 
 double to_24_hour_clock(double hours)
 {
@@ -100,6 +104,12 @@ double to_24_hour_clock(double hours)
         with integer and fractional part of a hours separately.
         
     */
+}
+
+
+int get_hours(int sec)
+{
+    return sec/3600;
 }
 
 
@@ -191,16 +201,19 @@ int main()
     assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON);
     assert(fabs(seconds_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
 
+
     //hours_difference 
     assert(fabs(hours_difference(1800.0, 3600.0) - 0.5) < DBL_EPSILON);
     assert(fabs(hours_difference(3600.0, 1800.0) + 0.5) < DBL_EPSILON);
     assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON);
     assert(fabs(hours_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
 
+
     //to_float_hours
     assert(fabs(to_float_hours(0, 15, 0) - 0.25) < DBL_EPSILON);
     assert(fabs(to_float_hours(2, 45, 9) - 2.7525) < DBL_EPSILON);
     assert(fabs(to_float_hours(1, 0, 36) - 1.01) < DBL_EPSILON);
+
 
     //to_24_hour_clock
     assert(fabs(to_24_hour_clock(24) - 0) < DBL_EPSILON);
@@ -208,5 +221,11 @@ int main()
     assert(fabs(to_24_hour_clock(25) - 1) < DBL_EPSILON);
     assert(fabs(to_24_hour_clock(4) - 4) < DBL_EPSILON);
     assert(fabs(to_24_hour_clock(28.5) - 4.5) < DBL_EPSILON);
+
+    //get_hours
+    assert(get_hours(3800) == 1);
+    assert(get_hours(7199) == 1);
+    assert(get_hours(10800) == 3);
+
 
 }
